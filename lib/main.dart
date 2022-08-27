b4dcf3a5d04d1711908d97aa4577cae7a935eff2
FlutLab.io <>
2022-08-27 06:21:02 +0000

    flutter login

--- lib/main.dart
+++ lib/main.dart
@@ -2,198 +2,76 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:http/http.dart' as http;
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(home: Login()));
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Wikipedia search API'),
-    );
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+  _LoginState createState() => _LoginState();
+}
+
+TextEditingController emailcontroler = TextEditingController();
+TextEditingController passwordcontroler = TextEditingController();
+
+void login(String email, String password) async {
+  try {
+    http.Response response = await http.post(
+        Uri.parse(" https://reqres.in/api/login"),
+        body: {"email": email, "password": password});
+
+    if (response.statusCode == 200) {
+      print("Logged in Sucefully");
+    } else {
+      print('Failed To login');
+    }
+  } catch (e) {
+    //print(e.tostring());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final searchTextController = TextEditingController();
-  List<WikiSearchEntity> searchList = [];
-
-  void _search() {
-    String str = searchTextController.text;
-    RequestService.query(str).then((WikiSearchResponse? response) {
-      setState(() {
-        searchList = response!.query.search;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    searchTextController.dispose();
-    super.dispose();
-  }
-
+class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: Colors.green,
+        title: Text('Login Page'),
       ),
-      body: Container(
-        child: Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: searchTextController,
-                        obscureText: false,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'TextField',
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Container(
-                      height: 60,
-                      child: OutlinedButton(
-                        onPressed: _search,
-                        child: Text("Search"),
-                      ),
-                    ),
-                  ]
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Expanded(child:
-                SingleChildScrollView(
-                child: ListView.builder(
-                  primary: false,
-                  itemBuilder: (BuildContext context,
-                      int index) => new WikiSearchItemWidget(searchList[index]),
-                  itemCount: searchList.length,
-                  shrinkWrap: true,
-                ),
-              ),
-              ),
-            ],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(20.0),
           ),
-        ),
+          TextFormField(
+            controller: emailcontroler,
+            decoration: InputDecoration(hintText: "email"),
+          ),
+          SizedBox(height: 40),
+          TextFormField(
+            controller: passwordcontroler,
+            decoration: InputDecoration(hintText: "Password"),
+          ),
+          SizedBox(height: 40),
+          GestureDetector(
+            onTap: () {
+              login(emailcontroler.text.toString(),
+                  passwordcontroler.text.toString());
+            },
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                  color: Colors.greenAccent,
+                  borderRadius: BorderRadius.circular(20.0)),
+              child: Text('Login'),
+            ),
+          )
+        ],
       ),
     );
   }
-}
-
-class WikiSearchItemWidget extends StatelessWidget {
-  final WikiSearchEntity _entity;
-
-  WikiSearchItemWidget(this._entity);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(_entity.title,
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w700,
-        ),
-      ),
-      subtitle: SingleChildScrollView(
-        child: Html(data: _entity.snippet),
-      ),
-      onTap: () {
-
-      },
-    );
-  }
-}
-
-class RequestService {
-  static Future<WikiSearchResponse?> query(String search) async {
-    var response = await http.get(
-        Uri.parse("https://en.wikipedia.org/w/api.php?action=query&origin=*&list=search&srsearch=$search&format=json")
-    );
-    // Check if response is success
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      var map = json.decode(response.body);
-      return WikiSearchResponse.fromJson(map);
-    } else {
-      print("Query failed: ${response.body} (${response.statusCode})");
-      return null;
-    }
-  }
-}
-
-class WikiSearchResponse {
-  String batchComplete;
-  WikiQueryResponse query;
-  WikiSearchResponse({required this.batchComplete, required this.query});
-
-  factory WikiSearchResponse.fromJson(Map<String, dynamic> json) => WikiSearchResponse(
-      batchComplete: json["batchcomplete"],
-      query: WikiQueryResponse.fromJson(json["query"])
-  );
-}
-
-class WikiQueryResponse {
-  List<WikiSearchEntity> search;
-
-  WikiQueryResponse({required this.search});
-
-  factory WikiQueryResponse.fromJson(Map<String, dynamic> json) {
-    List<dynamic> resultList = json['search'];
-    List<WikiSearchEntity> search = resultList.map((dynamic value) =>
-        WikiSearchEntity.fromJson(value))
-        .toList(growable: false);
-    return WikiQueryResponse(
-        search: search
-    );
-  }
-}
-
-class WikiSearchEntity {
-  int ns;
-  String title;
-  int pageId;
-  int size;
-  int wordCount;
-  String snippet;
-  String timestamp;
-  WikiSearchEntity({required this.ns, required this.title,
-    required this.pageId, required this.size, required this.wordCount,
-    required this.snippet, required this.timestamp});
-
-  factory WikiSearchEntity.fromJson(Map<String, dynamic> json) => WikiSearchEntity(
-    ns: json["ns"],
-    title: json["title"],
-    pageId: json["pageid"],
-    size: json["size"],
-    wordCount: json["wordcount"],
-    snippet: json["snippet"],
-    timestamp: json["timestamp"]
-  );
 }
